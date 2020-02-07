adcd54df6ff1056f8a284813b8b04a50122b9f51
Rodriguez Garcia Monica Haydee <mony_rgarcia@hotmail.com>
2020-02-07 10:57:48 -0600

    primera version

---
+++ src/test/java/com/pruebas/testNuevo/TestNuevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebas.testNuevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestNuevoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
